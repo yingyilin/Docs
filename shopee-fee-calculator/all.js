a84043f1abbcd90ef7a57159d1b819570dde6e64
yingyilin <57311407+yingyilin@users.noreply.github.com>
2024-12-31 22:38:33 +0800

    add 2025 promotion days alert

--- shopee-fee-calculator/all.js
+++ shopee-fee-calculator/all.js
@@ -1,3 +1,17 @@
+var promotionDay2025={
+    1: [11, 12, 18, 25], // 一月
+    2: [2, 18, 19, 25],  // 二月
+    3: [3, 4, 18, 25],   // 三月
+    4: [4, 18, 19, 25],  // 四月
+    5: [5, 6, 18, 25],   // 五月
+    6: [6, 18, 19, 25],  // 六月
+    7: [7, 8, 18, 25],   // 七月
+    8: [8, 9, 18, 25],   // 八月
+    9: [9, 10, 18, 25],  // 九月
+    10: [10, 11, 18, 25],// 十月
+    11: [11, 12, 18, 25],// 十一月
+    12: [12, 13, 18, 25] // 十二月
+};
 var inputPrice = document.getElementById('price');
 var inputQuantity = document.getElementById('quantity');
 var selectShippingFee = document.getElementById('shippingFee');
@@ -6,6 +20,7 @@ var isPromotion = document.getElementById('isPromotion');
 var hasDiscount = document.getElementById('hasDiscount');
 var isShippingFree = document.getElementById('isShippingFree');
 var calculateBtn = document.getElementById('calculateBtn');
+var  promotionAlert = document.getElementById('promotion-alert');
 
 //監聽
 calculateBtn.addEventListener('click', showResult);
@@ -76,3 +91,29 @@ function showResult(){
         calculateFees(0.055);
     }
 }
+
+//判斷今天是否為促銷日
+function isTodayPromotionDay() {
+  var today = new Date();
+  var currentMonth = today.getMonth() + 1;  // 取得當前月份
+  var currentDay = today.getDate();         // 取得當前日期
+
+
+    // 判斷本月是否有促銷日
+  var promotionDays = promotionDay2025[currentMonth] || [];
+  // if (!promotionDays) {
+  //     console.log("本月沒有促銷日");
+  //     return false;
+  // }
+
+  if (promotionDays.includes(currentDay)){
+      promotionAlert.style.display = 'block';
+      document.getElementById('promotion-alert-text').textContent = '今天是蝦皮促銷日，手續費率7.5%';
+      console.log("今天是促銷日");
+  } else {
+      console.log("今天不是促銷日");
+  }
+}
+
+isTodayPromotionDay();
+
